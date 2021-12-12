c94194888b90641aef570c21ea4bbffef4f84848
Hamza Mujeeb Khan <41168032+hamzamujeebk@users.noreply.github.com>
2021-12-12 15:27:39 +0530

    .

---
+++ build/precache-manifest.2569ed94e8e988852173b23b86111ba7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55bbd4e94f4303bccd09b22ba57b60dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9302e28affcbd310209f",
+    "url": "/static/js/2.17e1ab02.chunk.js"
+  },
+  {
+    "revision": "205c73c19f39324ab889",
+    "url": "/static/js/main.902dcaa4.chunk.js"
+  },
+  {
+    "revision": "164da21e09e43c8bb0c6",
+    "url": "/static/js/runtime-main.aa0ae0cd.js"
+  }
+]);
